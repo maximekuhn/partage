d225c9feba4016868439ba291903eb8481245a31
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2024-09-19 21:57:59 +0200

    fixed linter warnings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"log"
+
 	"github.com/maximekuhn/partage/internal/app/web"
 )
 
 func main() {
 	// FIXME: passing a nil will break, doing this for tmp testing...
 	s := web.NewServer(nil)
-	s.Run()
+	log.Fatal(s.Run())
 }
