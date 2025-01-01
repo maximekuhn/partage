1af001b64cc7f3262f7a0307056fa8590f18f7a3
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2025-01-01 22:09:53 +0100

    chore: refactored invite user to group to accept an email instead of a user id

--- internal/core/command/inviteuseringroup.go
+++ internal/core/command/inviteuseringroup.go
@@ -2,8 +2,7 @@ package command
 
 import (
 	"context"
-	"errors"
-	"time"
+	"fmt"
 
 	"github.com/maximekuhn/partage/internal/core/contract"
 	"github.com/maximekuhn/partage/internal/core/store"
@@ -12,37 +11,46 @@ import (
 
 type InviteUserInGroupCmd struct {
 	GroupID      valueobject.GroupID
-	UserToInvite valueobject.UserID
+	UserToInvite valueobject.Email
 }
 
 type InviteUserInGroupCmdHandler struct {
 	groupstore      store.GroupStore
+	userstore       store.UserStore
 	invitationstore store.InvitationStore
 	dtprovider      contract.DatetimeProvider
 }
 
-func NewInviteUserInGroupCmdHandler(groupstore store.GroupStore, invitationstore store.InvitationStore, dtprovider contract.DatetimeProvider) *InviteUserInGroupCmdHandler {
-	return &InviteUserInGroupCmdHandler{groupstore, invitationstore, dtprovider}
+func NewInviteUserInGroupCmdHandler(groupstore store.GroupStore, userstore store.UserStore, invitationstore store.InvitationStore, dtprovider contract.DatetimeProvider) *InviteUserInGroupCmdHandler {
+	return &InviteUserInGroupCmdHandler{groupstore, userstore, invitationstore, dtprovider}
 }
 
 func (h *InviteUserInGroupCmdHandler) Handle(ctx context.Context, cmd InviteUserInGroupCmd) error {
+	u, found, err := h.userstore.GetByEmail(ctx, cmd.UserToInvite)
+	if err != nil {
+		return err
+	}
+	if !found {
+		return fmt.Errorf("user not found for email %s", cmd.UserToInvite)
+	}
+
 	g, found, err := h.groupstore.FindByID(ctx, cmd.GroupID)
 	if err != nil {
 		return err
 	}
 	if !found {
-		return errors.New("group not found")
+		return fmt.Errorf("group not found for id %s", cmd.GroupID)
 	}
 
+	// check if the user is not already in the group
 	for _, member := range g.Members {
-		if member == cmd.UserToInvite {
-			return errors.New("user is already in the group")
+		if member == u.ID {
+			return fmt.Errorf("user with id %s is already in the group", u.ID)
 		}
 	}
 
-	// `UpdatedAt` field is set to a zero-valued time.Time because it has not been updated yet
-	// as the invitation is currently being created
+	// create and save pending invitation
 	now := h.dtprovider.Provide()
-	i := valueobject.NewInvitation(cmd.UserToInvite, cmd.GroupID, valueobject.InvitationStatusPending, now, time.Time{})
+	i := valueobject.NewInvitation(u.ID, g.ID, valueobject.InvitationStatusPending, now, now)
 	return h.invitationstore.Save(ctx, i)
 }
